f367a612db0d3160fc43b924d8d180f6e6ef1f8e
Misha Sereda <sereda2510@mail.ru>
2020-02-21 04:25:20 +0300

    конвертировал функиональную компоненту user в классовую

--- src/components/pages/Users/Users.component.jsx
+++ src/components/pages/Users/Users.component.jsx
@@ -4,39 +4,40 @@ import UserItem from './userItem/UserItem'
 import * as axios from "axios";
 
 
-const Users = (props) => {
+class Users extends React.Component {
 
-  if(props.users.length === 0) {
-    axios.get('https://social-network.samuraijs.com/api/1.0/users')
-    .then(response => {
-      debugger  
-     props.setUsers(response.data.items)
-    })
-  }
+  componentDidMount = () => {
+    if(this.props.users.length === 0) {
+      axios.get('https://social-network.samuraijs.com/api/1.0/users')
+      .then(response => {
+       this.props.setUsers(response.data.items)
+      })
+    }
+  } 
+  
+  render() {
+
 
     return(
-      
-       <table className={style.table}>
-          <tr className={style.table_header}>
-              <th>Name</th>
-              <th>City</th>
-              <th>E-mail</th>
-              <th>Phone number</th>
-              <th>Subcription</th>
-          </tr>
-          {props.users.map(user => (
-            <UserItem Follow={props.follow} Unfollow={props.unfollow} followed={user.followed}  img={user.photos.small} key={user.id} id={user.id} name={user.name} location={'user.location.city'} email={'user.email'} phone={'user.phone'} />
-          ))}
-      </table>
-
-    )
+      <table className={style.table}>
+         <tr className={style.table_header}>
+             <th>Name</th>
+             <th>City</th>
+             <th>E-mail</th>
+             <th>Phone number</th>
+             <th>Subcription</th>
+         </tr>
+         {this.props.users.map(user => (
+           <UserItem Follow={this.props.follow} Unfollow={this.props.unfollow} followed={user.followed}  img={user.photos.small} key={user.id} id={user.id} name={user.name} location={'user.location.city'} email={'user.email'} phone={'user.phone'} />
+         ))}
+     </table>
+   )
+  }
+    
 }
 
 export default Users;
 
-
-
-// 
 
 
 
