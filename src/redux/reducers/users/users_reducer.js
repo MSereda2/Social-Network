d6600e7fb0d0ab3e2a41504489635dbdfabe8b95
Misha Sereda <sereda2510@mail.ru>
2020-03-05 23:14:36 +0300

    small changes

--- src/redux/reducers/users/users_reducer.js
+++ src/redux/reducers/users/users_reducer.js
@@ -28,9 +28,9 @@ const user_reducer = (state = initialState, action) => {
                 ...state,
                 users: state.users.map(user => {
                     if(user.id === action.userID) {
-                        return {...user, followed : false}
+                        return {...user, followed: false}
                     }
-                    return user
+                    return user;
                 })
             })
         case userTypes.SET_USERS:
